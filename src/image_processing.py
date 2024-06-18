cc46c965fe36243bd94193254d55aee6de5f944e
rotemlevi <rotem85.levi@gmail.com>
2024-06-18 18:57:02 +0300

    test ci/cd

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageEnhance, ImageFilter
 
-def enhance_image(image):
+def enhance_image(image):#
     gray_image = image.convert('L')
     enhancer = ImageEnhance.Contrast(gray_image)
     enhanced_image = enhancer.enhance(2)
